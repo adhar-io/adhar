a01fb6f0d141cf3beef70c6df06f0b1514192a3c
Tapas Jena <tapas.friends@gmail.com>
2024-01-01 18:26:10 +0800

    chore(release): 0.1.9

--- platform/installer/digitalocean/helm.tf
+++ platform/installer/digitalocean/helm.tf
@@ -5,26 +5,27 @@ data "digitalocean_kubernetes_cluster" "cluster_info" {
   ]
 }
 
-# Installs Otomi chart on cluster
+# Installs Adhar chart on cluster
 # Note: This resource waits until all the jobs have finished installing.
-# Estimated time to finish for vanilla Otomi: 15 ~ 20min 
+# Estimated time to finish for vanilla Adhar: 15 ~ 20min 
 # If it takes longer than 20 minutes you might want to check the kubernetes dashboard for status 
-resource "helm_release" "otomi" {
-  name = "otomi"
+resource "helm_release" "adhar" {
+  name = "adhar"
 
-  repository = "https://otomi.io/otomi-core"
-  chart      = "otomi"
+  repository = "https://chart.adhar.io"
+  chart      = "adhar"
+  namespace  = "adhar-system"
 
   values        = [file("adhar-values.yaml")]
   timeout       = 1800
   wait_for_jobs = true
 }
 
-resource "null_resource" "print_otomi_url" {
+resource "null_resource" "print_adhar_url" {
   depends_on = [
-    helm_release.otomi
+    helm_release.adhar
   ]
   provisioner "local-exec" {
-    command = "kubectl logs jobs/otomi -n default -f"
+    command = "kubectl logs jobs/adhar -n adhar-system -f"
   }
 }
