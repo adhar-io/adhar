415f3fdeb53e932a05b69b9ef1b04b4b57483a2e
Tapas Jena <tapas.friends@gmail.com>
2024-09-29 23:12:16 +0800

    cleanup timoni and cue

---
+++ platform/stack/packages/data/cnpg/generate-manifests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+INSTALL_YAML="manifests/install.yaml"
+CHART_VERSION="v0.22.0"
+
+echo "# CLOUDNATIVE-PG INSTALL RESOURCES" >${INSTALL_YAML}
+echo "# This file is auto-generated with 'platform/stack/packages/data/cnpg/generate-manifests.sh'" >>${INSTALL_YAML}
+
+helm repo add cnpg https://cloudnative-pg.github.io/charts --force-update
+helm repo update
+helm template --namespace cnpg-system cnpg cnpg/cloudnative-pg -f values.yaml --version ${CHART_VERSION} >>${INSTALL_YAML}
