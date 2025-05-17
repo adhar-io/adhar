f73473dc14cd138df16f7db53394a4c5bcfd0529
Tapas Jena <tapas.friends@gmail.com>
2025-05-17 23:45:14 +0800

    sample CR added

--- hack/cilium/generate-manifests.sh
+++ hack/cilium/generate-manifests.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # Update Cilium manifest using Helm
-HACK_DIR="$(cd "$(dirname "$0")" && pwd)" # Ensure HACK_DIR is an absolute path
-CILIUM_VERSION=${1:-"1.13.0"} # Default to version 1.13.0 if not provided
-
-# Ensure the hack directory exists
-mkdir -p "$HACK_DIR"
+HACK_DIR="$(cd "$(dirname "$0")" && pwd)"
+CILIUM_VERSION="1.13.0"
+CILIUM_NAMESPACE="adhar-system"
 
 # Use Helm to generate the Cilium manifest including CRDs
 helm repo add cilium https://helm.cilium.io/
 helm repo update cilium
-helm template cilium cilium/cilium --version "$CILIUM_VERSION" --include-crds -f "$HACK_DIR/values.yaml" --output-dir "$HACK_DIR" > "$HACK_DIR/cilium.yaml"
+helm template cilium cilium/cilium --namespace $CILIUM_NAMESPACE --version "$CILIUM_VERSION" --include-crds -f "$HACK_DIR/values.yaml" --output-dir "$HACK_DIR" > "$HACK_DIR/install.yaml"
 
-if [ -f "$HACK_DIR/cilium.yaml" ]; then
+if [ -f "$HACK_DIR/install.yaml" ]; then
     echo "Cilium manifest with CRDs generated successfully."
 else
     echo "Failed to generate Cilium manifest with CRDs."
--- hack/crossplane/generate-manifests.sh
+++ hack/crossplane/generate-manifests.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # Update Crossplane manifest using Helm
-HACK_DIR="$(cd "$(dirname "$0")" && pwd)" # Ensure HACK_DIR is an absolute path
-CROSSPLANE_VERSION=${1:-"1.12.0"} # Default to version 1.12.0 if not provided
-
-# Ensure the hack directory exists
-mkdir -p "$HACK_DIR"
+HACK_DIR="$(cd "$(dirname "$0")" && pwd)"
+CROSSPLANE_VERSION="v1.19.0"
+CROSSPLANE_NAMESPACE="adhar-system"
 
 # Use Helm to generate the Crossplane manifest including CRDs
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update crossplane-stable
-helm template crossplane --namespace crossplane-system crossplane-stable/crossplane --version "$CROSSPLANE_VERSION" -f "$HACK_DIR/values.yaml" --output-dir "$HACK_DIR" > "$HACK_DIR/crossplane.yaml"
+helm template crossplane --namespace $CROSSPLANE_NAMESPACE crossplane-stable/crossplane --version "$CROSSPLANE_VERSION" -f "$HACK_DIR/values.yaml" --include-crds > "$HACK_DIR/install.yaml"
 
-if [ -f "$HACK_DIR/crossplane.yaml" ]; then
+if [ -f "$HACK_DIR/install.yaml" ]; then
     echo "Crossplane manifest with CRDs generated successfully."
 else
     echo "Failed to generate Crossplane manifest with CRDs."
     exit 1
 fi
 
-echo "Crossplane manifest updated to version $CROSSPLANE_VERSION."
+echo "Crossplane manifest updated to version $CROSSPLANE_VERSION in namespace $CROSSPLANE_NAMESPACE."
